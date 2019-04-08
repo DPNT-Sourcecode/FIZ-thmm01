2aa1599acaf7c31af3b532a10ee77e273aba48b3
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 16:23:15 +0000

    Mon Apr 08 16:23:15 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,7 +3,7 @@ class FizzBuzz
 
   def fizz_buzz(number)
 
-    if  (number % 15 == 0) && (number % 5 == 0 && number.to_s.include?('5')) || (number % 3 == 0 && number.to_s.include?('3'))
+    if  (number % 5 == 0 && number.to_s.include?('5')) || (number % 3 == 0 && number.to_s.include?('3'))
         if number % 2 != 0
          return "fizz buzz fake deluxe"
          else
@@ -11,18 +11,18 @@ class FizzBuzz
          end
          return "fizz buzz"
      elsif ((number % 5 == 0)|| number.to_s.include?('5'))
-        # if number % 2 != 0
-        #  return "buzz fake deluxe"
-        # else
-        #  return "buzz deluxe"
-        # end
+        if number % 2 != 0
+         return "buzz fake deluxe"
+        else
+         return "buzz deluxe"
+        end
       return "buzz"
     elsif ((number % 3 == 0) || number.to_s.include?('3'))
-        # if number % 2 != 0
-        #   return "fizz fake deluxe"
-        # else
-        #  return "fizz deluxe"
-        # end
+        if number % 2 != 0
+          return "fizz fake deluxe"
+        else
+         return "fizz deluxe"
+        end
       return "fizz"
     elsif ((number % 3 == 0) && number.to_s.include?('3')) || ((number % 5 == 0) && number.to_s.include?('5'))
       if number % 2 != 0
@@ -35,6 +35,7 @@ class FizzBuzz
     end
   end
 end
+
 
 
 
