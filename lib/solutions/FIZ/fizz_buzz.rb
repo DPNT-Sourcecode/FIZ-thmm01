169707c1d2e65bf7bbdf9fc7b64b2a953f198d1e
sbx_user1051 <sbx_user1051@169.254.227.221>
2019-04-08 21:54:47 +0000

    Mon Apr 08 21:54:47 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,6 +2,7 @@
 class FizzBuzz
 
   def fizz_buzz(number)
+
     if number % 15 == 0
       if (number % 5 == 0) || (number.to_s.include?('5')) && (number % 3 == 0) || (number.to_s.include?('3'))
       return "fizz buzz deluxe"
