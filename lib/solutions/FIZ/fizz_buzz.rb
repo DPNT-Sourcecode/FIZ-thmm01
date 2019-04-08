84e3e673ff1e094ce243155cb8777c4ea7da68ca
sbx_user1051 <sbx_user1051@169.254.227.221>
2019-04-08 22:09:47 +0000

    Mon Apr 08 22:09:47 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,10 +4,12 @@ class FizzBuzz
   def fizz_buzz(number)
 
     if number % 15 == 0
-      if (number % 5 == 0) && (number.to_s.include?('5')) || (number % 3 == 0) && (number.to_s.include?('3'))
-      return "fizz buzz deluxe"
-      else
       return "fizz buzz"
+    elsif (number % 5 == 0) && (number.to_s.include?('5')) || (number % 3 == 0) && (number.to_s.include?('3'))
+      if number % 2 != 0
+      return "fizz buzz fake deluxe"
+      else
+      return "fizz buzz deluxe"
       end
     elsif (number % 5 == 0) && (number.to_s.include?('5'))
       if (number % 2 != 0)
@@ -23,18 +25,13 @@ class FizzBuzz
       return "fizz deluxe"
       end
       return "fizz"
-    elsif (number % 5 == 0) && (number.to_s.include?('5')) || (number % 3 == 0) && (number.to_s.include?('3'))
-      if (number % 2 != 0)
-        return "fake deluxe"
-      else
-      return "deluxe"
-      end
-  elsif (number % 15 == 0) && (number % 2 != 0)
-    return "fizz buzz fake deluxe"
+    elsif number % 2 != 0
+      return "fake deluxe"
     else
       return number
     end
   end
 end
+
 
 
