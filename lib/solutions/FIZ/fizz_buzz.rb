46301cec8991c3f4f226664105642e3f3ecf0c6c
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 16:43:16 +0000

    Mon Apr 08 16:43:16 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -10,7 +10,7 @@ class FizzBuzz
          return "fizz buzz deluxe"
          end
          return "fizz buzz"
-    elsif ((number % 5 == 0) && number.to_s.include?('5'))
+    elsif ((number % 5 == 0) || number.to_s.include?('5'))
         if number % 2 != 0
          return "buzz fake deluxe"
         else
@@ -18,7 +18,7 @@ class FizzBuzz
         end
       return "buzz"
 
-    elsif ((number % 3 == 0) && number.to_s.include?('3'))
+    elsif ((number % 3 == 0) || number.to_s.include?('3'))
         if number % 2 != 0
           return "fizz fake deluxe"
         else
@@ -26,8 +26,7 @@ class FizzBuzz
        end
       return "fizz"
 
-    elsif (number > 10) && (number.to_s.chars.uniq.length == 1)
-      if number % 2 != 0
+    elsif number % 2 != 0
       return "fake deluxe"
     else
       return "deluxe"
@@ -37,6 +36,5 @@ class FizzBuzz
     end
   end
 
-end
 
 
