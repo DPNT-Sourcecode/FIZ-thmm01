88ca2dafd5adbc1aaeaf8c5256bbbede5dc4fa85
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 15:41:01 +0000

    Mon Apr 08 15:41:01 UTC 2019

--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -13,7 +13,7 @@ class ClientTest < Minitest::Test
     assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(546), 'App should return fizzbuzz'
     assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'App should return buzz'
     assert_equal 'buzz deluxe', FizzBuzz.new.fizz_buzz(55), 'App should return buzz deluxe'
-    assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(555)
+    assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(555), 'App should return buzz fake deluxe'
     assert_equal 'fizz deluxe', FizzBuzz.new.fizz_buzz(33), 'App should return fizz deluxe'
     assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(777), 'App should return fizz fake deluxe'
     assert_equal 'fizz', FizzBuzz.new.fizz_buzz(3), 'App should return fizz'
@@ -22,4 +22,5 @@ class ClientTest < Minitest::Test
   end
 
 end
+
 
