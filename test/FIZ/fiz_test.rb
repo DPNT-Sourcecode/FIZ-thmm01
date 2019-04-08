e8388ff9dcc53a59dc5dceb2a99e0e4253dc62f9
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:38:43 +0000

    Mon Apr 08 13:38:43 UTC 2019

--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -13,7 +13,9 @@ class ClientTest < Minitest::Test
     assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(546), 'App should return fizzbuzz'
     assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'App should return buzz'
     assert_equal 'fizz', FizzBuzz.new.fizz_buzz(3), 'App should return fizz'
+    assert_equal 'deluxe', FizzBuzz.new.fizz_buzz(11), 'App should return deluxe'
     assert_equal 1, FizzBuzz.new.fizz_buzz(1), 'App should return num'
   end
 
 end
+
