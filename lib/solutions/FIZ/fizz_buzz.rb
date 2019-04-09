7b39c2c6471960659192265171a2112e1540af9e
sbx_user1051 <sbx_user1051@169.254.196.241>
2019-04-09 11:35:47 +0000

    Tue Apr 09 11:35:47 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,9 +1,32 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
-
   def fizz_buzz(number)
-    return "fizz buzz" if number % 15 == 0
+    if (number % 2 != 0 && number % 15 == 0) && ((number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
+      return 'fizz buzz fake deluxe'
+    elsif (number % 2 == 0 && number % 15 == 0) && ((number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
+      return 'fizz buzz deluxe'
+    elsif number % 15 == 0 || (number % 3 == 0 && number.to_s.include?('5')) || (number % 5 == 0 && number.to_s.include?('3')) || (number.to_s.include?('3') && number.to_s.include?('5'))
+      return 'fizz buzz'
+    elsif  number % 2 != 0 && (number % 3 == 0 && number.to_s.include?('3'))
+        return 'fizz fake deluxe'
+    elsif  number % 2 != 0 && (number % 5 == 0 && number.to_s.include?('5'))
+        return 'buzz fake deluxe'
+    elsif  number % 2 == 0 && (number % 3 == 0 && number.to_s.include?('3'))
+        return 'fizz deluxe'
+    elsif  number % 2 == 0 && (number % 5 == 0 && number.to_s.include?('5'))
+        return 'buzz deluxe'
+    elsif number % 2 != 0 && ((number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
+          return 'fake deluxe'
+    elsif number % 2 == 0 && ((number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
+        return 'deluxe'
+    elsif number % 3 == 0 || number.to_s.include?('3')
+      return 'fizz'
+    elsif number % 5 == 0 || number.to_s.include?('5')
+      return 'buzz'
+    else
+      return number.to_s
+    end
   end
-
 end
+
 
